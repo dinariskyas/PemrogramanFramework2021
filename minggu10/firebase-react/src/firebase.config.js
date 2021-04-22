2335100d47ad49a671aacebedfbb5f5d8846f7ac
dinariskyas <dinarisky04@gmail.com>
2021-04-23 03:59:03 +0700

    add modul 10

---
+++ minggu10/firebase-react/src/firebase.config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAxN01YcknMBOanPRria_XO0yyo9fOFp84",
+    authDomain: "fir-reactjs-dinariskyas.firebaseapp.com",
+    projectId: "fir-reactjs-dinariskyas",
+    storageBucket: "fir-reactjs-dinariskyas.appspot.com",
+    messagingSenderId: "100956294020",
+    appId: "1:100956294020:web:19ae121ed6d9a8545debd1",
+    measurementId: "G-CGFHGE3649"
+};
+
+export default firebaseConfig;
